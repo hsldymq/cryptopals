fc8020075dbfde8197e93c53ec637f15a0edd3ff
hsldymq <hsldymq@qq.com>
2018-12-01 16:45:58 +0800

    finish set 1 challenge 1

--- src/set1.rs
+++ src/set1.rs
@@ -1,43 +1,52 @@
-use std::cmp; 
+use std::cmp;
 
-pub fn challenge1(hex: &str) -> String {
+// challenge 1
+pub fn hex_to_base64(hex: &str) -> String {
+    let len = hex.len();
+    let mut bytes: Vec<u8> = Vec::with_capacity((len + 1) / 2);
+    let mut from = 0;
+    let mut to = cmp::min(len, 2);
+
+    while from < len {
+        bytes.push(u8::from_str_radix(&hex[from..to], 16).unwrap());
+
+        from = to;
+        to = cmp::min(len, to + 2);
+    }
+
+    base64_encode(&bytes)
+}
+
+pub fn base64_encode(bytes: &[u8]) -> String {
     let table = [
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
         'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 
         'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
         'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/',
     ];
-    // 
     let padding = ["", "", "==", "", "=", ""];
 
     let mut hold = 0u8; 
-    let mut offset = 0; 
+    let mut offset = 2; 
     let mut code: u8;
-    
-    let len = hex.len();
-    // hex转换为bytes之后的位长度
-    let bit_len = if len % 2 == 1 { len + 1 } else { len } * 4;
+
+    let bit_len = bytes.len() * 8;
     let mut result = String::with_capacity((bit_len + 5) / 6 + padding[bit_len % 6].len());
 
-    let mut from = 0;
-    let mut to = cmp::min(len, 2);
-    while to <= len {
-        let parsed = u8::from_str_radix(&hex[from..to], 16).unwrap();
-
-        code = ((parsed << 0) | hold) & 0b0011_1111;
-        hold = parsed >> (6 - offset);
+    for b in bytes {
+        code = hold | (b >> offset);
+        hold = (b << (6 - offset)) & 0b0011_1111u8;
         offset += 2;
         result.push(table[code as usize]);
 
-        if offset == 6 {
+        if offset == 8 {
             result.push(table[hold as usize]);
             hold = 0;
-            offset = 0;
+            offset = 2;
         }
-        
-
-        from = to;
-        to = cmp::min(len, to + 2);
+    }
+    if hold > 0 {
+        result.push(table[hold as usize]);
     }
 
     result.push_str(padding[bit_len % 6]);
@@ -51,8 +60,20 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_challenge1() {
-        let base64_value = challenge1("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
+    fn test_base64_encode() {
+        assert_eq!(base64_encode("".as_bytes()), "");
+        assert_eq!(base64_encode("f".as_bytes()), "Zg==");
+        assert_eq!(base64_encode("fo".as_bytes()), "Zm8=");
+        assert_eq!(base64_encode("foo".as_bytes()), "Zm9v");
+        assert_eq!(base64_encode("foob".as_bytes()), "Zm9vYg==");
+        assert_eq!(base64_encode("fooba".as_bytes()), "Zm9vYmE=");
+        assert_eq!(base64_encode("foobar".as_bytes()), "Zm9vYmFy");
+    }
+
+    #[test]
+    fn test_hex_to_base64() {
+        let base64_value = hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
         assert_eq!(base64_value, "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
     }
+    
 }
