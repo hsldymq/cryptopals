41ed63df0c21726aadc3a6f85b10b07287955132
hsldymq <hsldymq@qq.com>
2019-01-31 01:22:08 +0800

    添加注释

--- src/set1.rs
+++ src/set1.rs
@@ -3,7 +3,11 @@ use std::io::{Error, ErrorKind};
 use std::collections::HashMap;
 use super::*;
 
-// challenge 1
+// challenge 1: Convert hex to base64
+// The string:
+//  49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+// Should produce:
+//  SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
 pub fn hex_to_base64(hex: &str) -> Result<String, Error> {
     let len = hex.len();
     let mut bytes: Vec<u8> = Vec::with_capacity((len + 1) / 2);
@@ -24,7 +28,14 @@ pub fn hex_to_base64(hex: &str) -> Result<String, Error> {
     Ok(base64_encode(&bytes))
 }
 
-// challenge 2
+// challenge 2: Fixed XOR
+// Write a function that takes two equal-length buffers and produces their XOR combination.
+// If your function works properly, then when you feed it the string:
+//  1c0111001f010100061a024b53535009181c
+// ... after hex decoding, and when XOR'd against:
+//  686974207468652062756c6c277320657965
+// ... should produce:
+//  746865206b696420646f6e277420706c6179
 pub fn fixed_xor(a: &str, b: &str) -> Result<String, Error> {
     if a.len() != b.len() {
         return Err(Error::new(ErrorKind::InvalidInput, "Length Not Equal"));
@@ -44,7 +55,11 @@ pub fn fixed_xor(a: &str, b: &str) -> Result<String, Error> {
     Ok(result)
 }
 
-// challenge 3
+// challenge 3: Single-byte XOR cipher
+// The hex encoded string:
+//  1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736
+// ... has been XOR'd against a single character. Find the key, decrypt the message.
+//
 // Key: 0x58(X)
 // 加密文本: Cooking MC's like a pound of bacon 
 // BTW: Cooking MC's like a pound of bacon是Vanilla Ice在80年代的一首说唱歌曲Ice Ice Baby里的歌词
@@ -104,7 +119,7 @@ pub fn single_byte_xor_cipher(encrypted: &str) {
     }
 }
 
-// challenge 4
+// challenge 4: Detect single-character XOR
 // Key: 0x35(5)
 // 被加密行: 7b5a4215415d544115415d5015455447414c155c46155f4058455c5b523f
 // 加密文本: Now that the party is jumping\n
@@ -446,7 +461,15 @@ pub fn detect_single_character_xor() {
     }
 }
 
-// chanllenge 5
+// chanllenge 5: Implement repeating-key XOR
+// Here is the opening stanza of an important work of the English language:
+//  Burning 'em, if you ain't quick and nimble
+//  I go crazy when I hear a cymbal
+// Encrypt it, under the key "ICE", using repeating-key XOR.
+// In repeating-key XOR, you'll sequentially apply each byte of the key; the first byte of plaintext will be XOR'd against I, the next C, the next E, then I again for the 4th byte, and so on.
+// It should come out to:
+//  0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272
+//  a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f
 pub fn repeating_key_xor(text: &str, key: &str) -> String {
     let vec = repeat_xor_encrypt(text.as_bytes(), key.as_bytes());
     let mut result = String::new();
